b2e4b7e9245fe13b8793b82881f197e83bf2011a
Nick Lawrence <ntl@us.ibm.com>
2023-06-06 11:40:12 -0500

    cleanup

---
+++ pytorch_distributed/mnist/mnist.py
@@ -0,0 +1,156 @@
+# https://kengz.gitbook.io/blog/ml/distributed-training-with-torchelastic-on-kubernetes#pytorch-lightning
+# https://lightning.ai/docs/pytorch/stable/notebooks/lightning_examples/mnist-hello-world.html
+# https://lightning.ai/docs/pytorch/stable/common/checkpointing_basic.html
+# https://lightning.ai/docs/pytorch/stable/accelerators/gpu_intermediate.html#torch-distributed-elastic
+# https://lightning.ai/docs/pytorch/stable/common/checkpointing_basic.html#save-a-checkpoint
+# https://github.com/Lightning-AI/lightning/discussions/7186#discussioncomment-654431
+import argparse
+import pytorch_lightning as L
+import torch
+from torch.nn import functional as F
+from torchvision import transforms
+from torchmetrics import F1Score
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from typing import Optional
+import logging
+import sys
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s : %(message)s",
+    level=logging.INFO,
+    stream=sys.stdout,
+)
+
+ROOT_DIR = "/home/jovyan/kubeflow-ppc64le-examples/pytorch_distributed/working_root"
+
+
+class MNISTDataModule(L.LightningDataModule):
+    """
+    Data Module to load MNIST dataset
+
+    Splits into train/validate/test sets
+    """
+
+    def __init__(self, data_dir: str, batch_size: int):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.data = {}
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            mnist_full = MNIST(
+                self.data_dir, download=True, train=True, transform=self.transforms
+            )
+            self.data["train"], self.data["val"] = random_split(
+                mnist_full, [55000, 5000]
+            )
+        elif stage == "test":
+            self.data["test"] = MNIST(
+                self.data_dir, download=True, train=False, transform=transforms
+            )
+
+    def train_dataloader(self):
+        return DataLoader(self.data["train"], batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.data["test"], batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.data["val"], batch_size=self.batch_size)
+
+
+class MNISTModel(L.LightningModule):
+    """
+    Minimal MNIST classifier
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        # Model
+        self.fc = torch.nn.Linear(28 * 28, 10)
+
+        # Metrics
+        self.val_f1 = F1Score(task="multiclass", num_classes=10)
+        self.test_f1 = F1Score(task="multiclass", num_classes=10)
+
+    def forward(self, x):
+        flatten = x.view(x.size(0), -1)
+        return torch.relu(self.fc(flatten))
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        loss = F.cross_entropy(self(x), y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        preds = torch.argmax(logits, dim=1)
+        self.val_f1.update(preds, y)
+        self.log("val_F1", self.val_f1, on_epoch=True)
+
+    def on_validation_epoch_end(self):
+        metrics = self.trainer.callback_metrics
+        log.info(f'val_F1 = {metrics["val_F1"]}')
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        preds = torch.argmax(logits, dim=1)
+        self.test_f1.update(preds, y)
+        self.log("test_F1", self.test_f1, on_epoch=True)
+
+    def on_test_epoch_end(self):
+        metrics = self.trainer.callback_metrics
+        log.info(f'test_F1 = {metrics["test_F1"]}')
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.02)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir", type=str, default=f"{ROOT_DIR}/data", help="Data Directory"
+    )
+    parser.add_argument(
+        "--root_dir", type=str, default=f"{ROOT_DIR}/root", help="Root Directory"
+    )
+    parser.add_argument("--model", type=str, default=f"{ROOT_DIR}/model.pt")
+    parser.add_argument("--max_epochs", type=int, default=1)
+    parser.add_argument(
+        "--batch_size", type=int, default=256 if torch.cuda.is_available() else 64
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+
+    args = parse_args()
+
+    model = MNISTModel()
+    mnist = MNISTDataModule(data_dir=args.data_dir, batch_size=args.batch_size)
+
+    # Initialize a trainer
+    trainer = L.Trainer(
+        accelerator="auto",
+        devices=torch.cuda.device_count if torch.cuda.is_available() else 1,
+        max_epochs=args.max_epochs,
+        default_root_dir=args.root_dir,
+        enable_checkpointing=False,
+        limit_train_batches=0.25,
+    )
+
+    # Train the model
+    trainer.fit(model, mnist)
+
+    torch.save(model, args.model)
---
+++ pytorch_distributed/pytorch_distributed_kf_tools/src/pytorch_distributed_kf_tools/deploy.py
@@ -0,0 +1,253 @@
+import os
+import subprocess
+import shutil
+from dataclasses import dataclass
+
+from typing import Optional, List, Dict
+
+from kubernetes.client import (
+    V1PodTemplateSpec,
+    V1ObjectMeta,
+    V1PodSpec,
+    V1Container,
+    V1EnvVar,
+    V1ResourceRequirements,
+    V1VolumeMount,
+    V1Volume,
+    V1PersistentVolumeClaimVolumeSource,
+    V1EmptyDirVolumeSource,
+    V1OwnerReference,
+    V1ObjectFieldSelector,
+    V1EnvVarSource,
+)
+
+from kubeflow.training import (
+    V1ReplicaSpec,
+    KubeflowOrgV1PyTorchJob,
+    KubeflowOrgV1PyTorchJobSpec,
+    KubeflowOrgV1ElasticPolicy,
+    V1RunPolicy,
+    TrainingClient,
+)
+
+from kubeflow.training.constants import constants
+from kubernetes import client, config, watch
+
+DEFAULT_CONTAINER_ENV = (
+    [
+        V1EnvVar(name="LOGLEVEL", value="DEBUG"),
+        V1EnvVar(name="NCCL_DEBUG", value="DEBUG"),
+        V1EnvVar(name="TORCH_CPP_LOG_LEVEL", value="INFO"),
+        V1EnvVar(name="C10D_DEBUG_MODE", value="DETAIL"),
+        V1EnvVar(name="PET_VERBOSE", value="1"),
+    ],
+)
+
+
+@dataclass
+class OwningWorkFlow:
+    name: str
+    uid: str
+
+
+def wait_for_pod_ready(name: str, namespace: str):
+    """Waits for a Pod to become ready.
+    At that point all containers in the pod have been started
+    """
+    config.load_incluster_config()
+    w = watch.Watch()
+    core_v1 = client.CoreV1Api()
+
+    # Watching a specific pod is done with a field selector on the name.
+    # https://github.com/kubernetes-client/python/issues/467
+    for event in w.stream(
+        func=core_v1.list_namespaced_pod,
+        namespace=namespace,
+        field_selector=f"metadata.name={name}",
+        timeout_seconds=120,
+    ):
+        # Phases: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase
+        if event["object"].status.phase not in {"Pending"}:
+            w.stop()
+            return
+        # event.type: ADDED, MODIFIED, DELETED
+        if event["type"] == "DELETED":
+            print(f" {name} deleted before it started")
+            w.stop()
+            return
+
+
+def run_pytorch_job(
+    namespace: str,
+    pytorch_job_name: str,
+    shared_pvc_name: str,
+    shared_pvc_mount_point: str,
+    owning_workflow: Optional[OwningWorkFlow],
+    command: List[str],
+    num_workers: int,
+    gpus_per_worker: int = 1,
+    env: Optional[Dict[str, str]] = None,
+    working_dir: Optional[str] = None,
+    worker_image: str = "quay.io/ntlawrence/pytorchv1.13:latest",
+    shared_pvc_subpath: Optional[str] = None,
+    completion_timeout: int = 60 * 60 * 24,
+):
+    # An owner reference for the workflow
+    # When the workflow is deleted, the torch job is
+    # garbage collected.
+    workflow_ownership = []
+    if owning_workflow:
+        workflow_ownership = [
+            V1OwnerReference(
+                api_version="v1",
+                kind="Workflow",
+                name=owning_workflow.name,
+                uid=owning_workflow.uid,
+            )
+        ]
+
+    container_env = (
+        [V1EnvVar(n, v) for n, v in env.items()] if env else DEFAULT_CONTAINER_ENV
+    )
+    resources = (
+        V1ResourceRequirements(limits={"nvidia.com/gpu": f"{gpus_per_worker}"})
+        if gpus_per_worker
+        else None
+    )
+
+    container_working_dir = working_dir if working_dir else shared_pvc_mount_point
+
+    # Pod definition for each worker replica
+    # Defines the container image, command, and volume mounts
+    pod_template = V1PodTemplateSpec(
+        metadata=V1ObjectMeta(
+            name=pytorch_job_name,
+            namespace=namespace,
+            owner_references=workflow_ownership,
+            # https://github.com/kubeflow/website/issues/2011
+            annotations={"sidecar.istio.io/inject": "false"},
+        ),
+        spec=V1PodSpec(
+            containers=[
+                V1Container(
+                    name=constants.PYTORCHJOB_CONTAINER,
+                    image=worker_image,
+                    image_pull_policy="IfNotPresent",
+                    working_dir=container_working_dir,
+                    command=command,
+                    env=container_env,
+                    resources=resources,
+                    volume_mounts=[
+                        V1VolumeMount(
+                            mount_path=shared_pvc_mount_point,
+                            name="shared",
+                            sub_path=shared_pvc_subpath,
+                        ),
+                        # PyTorch requires shared memory on each pod
+                        V1VolumeMount(mount_path="/dev/shm", name="dshm"),
+                    ],
+                )
+            ],
+            volumes=[
+                V1Volume(
+                    name="shared",
+                    persistent_volume_claim=V1PersistentVolumeClaimVolumeSource(
+                        claim_name=shared_pvc_name
+                    ),
+                ),
+                V1Volume(
+                    name="dshm", empty_dir=V1EmptyDirVolumeSource(medium="Memory")
+                ),
+            ],
+        ),
+    )
+
+    # The PyTorchJob supports the concept of a master replica, but not
+    # specified the first worker takes that role. Having only workers
+    # simplifies the template a bit. This is how the imagenet example is
+    # setup. https://github.com/kubeflow/training-operator/blob/master/examples/pytorch/elastic/imagenet/
+    worker_spec = V1ReplicaSpec(
+        replicas=num_workers, restart_policy="Never", template=pod_template
+    )
+
+    # The owner references and replica spec are used to define the torch job
+    pytorchjob = KubeflowOrgV1PyTorchJob(
+        api_version=f"{constants.KUBEFLOW_GROUP}/{constants.OPERATOR_VERSION}",
+        kind=constants.PYTORCHJOB_KIND,
+        metadata=V1ObjectMeta(
+            name=pytorch_job_name, owner_references=[workflow_ownership]
+        ),
+        spec=KubeflowOrgV1PyTorchJobSpec(
+            # c10d is the most commonly used because it doesn't require additional
+            # packages. The primary advantage that elastic solutions offer is the
+            # ability to use cheap hardware in the cloud that can be taken away
+            # at any time. That doesn't apply so much for Power servers that are
+            # running on on-premise. Here we default to a fixed size of the
+            # number of replicias.
+            elastic_policy=KubeflowOrgV1ElasticPolicy(
+                rdzv_backend="c10d",
+                rdzv_id=pytorch_job_name,
+                n_proc_per_node=gpus_per_worker,
+                min_replicas=num_workers,
+                max_replicas=num_workers,
+                max_restarts=0,
+            ),
+            run_policy=V1RunPolicy(clean_pod_policy="None"),
+            pytorch_replica_specs={"Worker": worker_spec},
+        ),
+    )
+
+    ##########################
+    # Submit training job
+    ##########################
+    training_client = TrainingClient()
+    training_client.create_pytorchjob(pytorchjob)
+
+    try:
+        training_client.wait_for_job_conditions(
+            pytorch_job_name,
+            expected_conditions={
+                constants.JOB_CONDITION_RUNNING,
+                constants.JOB_CONDITION_SUCCEEDED,
+                constants.JOB_CONDITION_FAILED,
+            },
+            job_kind=constants.PYTORCHJOB_KIND,
+            timeout=600,
+        )
+    except RuntimeError as e:
+        # https://github.com/kubeflow/training-operator/issues/1806#issue-1708084586
+        pass
+
+    # Wait for pods to be ready, must do this before reading logs
+    pod_names = training_client.get_job_pod_names(name=pytorch_job_name, is_master=None)
+    for pod in pod_names:
+        wait_for_pod_ready(pod)
+
+    # stream logs for all workers
+    # (most of the interesting stuff is in worker 0)
+    training_client.get_job_logs(
+        name=pytorch_job_name,
+        is_master=False,
+        container=constants.PYTORCHJOB_CONTAINER,
+        follow=True,
+    )
+
+    # No more logs means workers have finished, wait for the rest of the job
+    try:
+        training_client.wait_for_job_conditions(
+            pytorch_job_name,
+            expected_conditions={
+                constants.JOB_CONDITION_SUCCEEDED,
+                constants.JOB_CONDITION_FAILED,
+            },
+            timeout=completion_timeout,
+            job_kind=constants.PYTORCHJOB_KIND,
+        )
+    except RuntimeError as e:
+        # https://github.com/kubeflow/training-operator/issues/1806#issue-1708084586
+        pass
+
+    if training_client.is_job_failed(
+        name=pytorch_job_name, job_kind=constants.PYTORCHJOB_KIND
+    ):
+        raise RuntimeError(f"Job {pytorch_job_name} Failed!")
