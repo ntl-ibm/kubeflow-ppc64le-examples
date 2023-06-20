5924880dd33a5e70bd11a548a01a41e3e9039a08
Nick Lawrence <ntl@us.ibm.com>
2023-06-20 15:22:57 -0500

    fixes

--- distributed_training/pytorch/pytorch_distributed_kf_tools/src/pytorch_distributed_kf_tools/deploy.py
+++ distributed_training/pytorch/pytorch_distributed_kf_tools/src/pytorch_distributed_kf_tools/deploy.py
@@ -459,18 +459,6 @@ def run_pytorch_job(
             logger.debug(yaml.dump(pytorchjob.to_dict()))
 
         training_client = TrainingClient()
-
-        existing_jobs = {
-            job.metadata.name
-            for job in training_client.list_pytorchjobs(namespace=namespace)
-        }
-        if pytorch_job_name in existing_jobs:
-            training_client.delete_pytorchjob(
-                pytorch_job_name,
-                namespace=namespace,
-                delete_options=V1DeleteOptions(propagation_policy="Foreground"),
-            )
-
         training_client.create_pytorchjob(pytorchjob)
 
         _wait_for_job_conditions(
