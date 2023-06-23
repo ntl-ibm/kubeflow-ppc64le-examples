6754964b25ac2e916bf1daadb273bb2847c3030d
Nick Lawrence <ntl@us.ibm.com>
2023-06-23 15:07:35 -0500

    fixes

--- distributed_training/distributed_kf_tools/src/distributed_kf_tools/event_logger.py
+++ distributed_training/distributed_kf_tools/src/distributed_kf_tools/event_logger.py
@@ -32,15 +32,15 @@ Example:
 
 Author: ntl@us.ibm.com
 """
+from dataclasses import dataclass
 from datetime import datetime
 import http.client
 import logging
 import os
 import multiprocessing
-from typing import Set, NamedTuple
-
+from typing import Set, List, NamedTuple, Optional, Generator
 from kubernetes import client, watch
-from kubernetes.client import ApiException, CoreV1Event
+from kubernetes.client import ApiException, CoreV1Event, CoreV1EventList, CoreV1Api
 
 logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get("EVENT_LOGGER_LOGLEVEL", "INFO"))
@@ -60,6 +60,115 @@ class InvolvedObject(NamedTuple):
             (event.involved_object.kind == self.kind)
             and (event.involved_object.name == self.name)
         )
+
+
+@dataclass
+class WatchState(NamedTuple):
+    """This class tracks the current list of events, and which ones have been processed
+
+    It includes changes to watch for new events, and process those new events as they
+    happen.
+    """
+
+    namespace: str
+    resource: CoreV1EventList
+    processed_uids: Set[str]
+
+    @property
+    def unprocessed(self) -> Generator[CoreV1Event, None, None]:
+        if self.resource.items:
+            for core_event in self.resource.items:
+                if (
+                    core_event.metadata
+                    and core_event.metadata.uid
+                    and core_event.metadata.uid not in self.processed_uids
+                ):
+                    yield core_event
+
+    def mark_all_events_as_processed(self) -> None:
+        for core_event in self.unprocessed:
+            self.mark_event_processed(core_event)
+
+    def stream_unprocessed_events(
+        self, api: CoreV1Api, timeout: int
+    ) -> Generator[CoreV1Event, None, None]:
+        """
+        Streams unprocessed events, up until the timeout.
+
+        Unprocessed events can come from one of two sources:
+        * Events in the event list from the last reload that have not been processed
+        * Events that happen after the last reload while we are watching
+
+        When the iteration returns to the generator for the next item, the
+        core event is marked as processed.
+        """
+        for core_event in self.unprocessed:
+            yield core_event
+            self.mark_event_processed(core_event)
+
+        w = watch.Watch()
+        try:
+            for event in w.stream(
+                api.list_namespaced_event,
+                namespace=self.namespace,
+                timeout_seconds=timeout,
+                resource_version=self.version,
+            ):
+                if not isinstance(event, dict):
+                    logger.warn(
+                        f"Attempt to process an event that was type {type(event)} when dict was expected. {event}"
+                    )
+                    continue
+
+                if event["type"] == "ADDED":
+                    core_event = event["object"]
+                    if not self.has_processed(core_event):
+                        yield core_event
+                        self.mark_event_processed(core_event)
+
+        except ApiException as e:
+            # It's possible that the watched version might not be valid anymore, since watches are only
+            # kept around for a short period of time. If that happens, treat it like a timeout
+            # The caller will reload and stream again
+            if e.status == http.client.GONE:
+                logger.debug(f"Watch API returned GONE while monitoring events")
+            else:
+                raise
+
+    @property
+    def version(self) -> Optional[int]:
+        if self.resource.metadata:
+            return self.resource.metadata.resource_version
+        return None
+
+    @classmethod
+    def initialize(cls, namespace: str, api: CoreV1Api) -> "WatchState":
+        try:
+            resource = api.list_namespaced_event(namespace)
+        except ApiException as e:
+            logger.warn(f"Error when retrieving namespaced events {e}")
+            resource = CoreV1EventList()
+
+        return WatchState(namespace=namespace, resource=resource, processed_uids=set())
+
+    def mark_event_processed(self, core_event: CoreV1Event):
+        if core_event.metadata and core_event.metadata.uid:
+            self.processed_uids.add(core_event.metadata.uid)
+        else:
+            pass
+
+    def has_processed(self, core_event: CoreV1Event) -> bool:
+        if core_event.metadata and core_event.metadata.uid:
+            return core_event.metadata.uid in self.processed_uids
+        else:
+            return False
+
+    def reload_event_list(self, api: CoreV1Api) -> None:
+        try:
+            resource = api.list_namespaced_event(self.namespace)
+        except ApiException as e:
+            logger.warn(f"Error when reloading namespaced events {e}")
+            resource = CoreV1EventList()
 
 
 class EventLogger:
@@ -86,6 +195,7 @@ class EventLogger:
         self.namespace = namespace
         self.involved_objects = involved_objects
         self.stop_monitoring = multiprocessing.Event()
+        self.is_monitoring = multiprocessing.Event()
         self.process = multiprocessing.Process(
             target=lambda: self._watch_events(), daemon=False
         )
@@ -138,61 +248,48 @@ class EventLogger:
             is not None
         )
 
+    def _process_event(self, core_event: CoreV1Event) -> None:
+        """Processes an event (if relevant) by logging it to the logger"""
+        if self._is_relevant(core_event):
+            logger.log(
+                EventLogger._set_log_level(core_event),
+                EventLogger._build_msg(core_event),
+            )
+
     def _watch_events(self):
         """Watches for and logs relevant events
 
         This method runs in a secondary thread until it is signaled that it should stop
         """
+
         logger.debug(
             f"Watching events for {self.involved_objects} in process {self.process.pid}"
         )
-        w = watch.Watch()
         api = client.CoreV1Api()
-        resource_version = None
+        state = WatchState.initialize(self.namespace, api)
+
+        # Ignore events from before we started monitoring, these can happen if a pod name is reused
+        # Example: Someone terminates a pipeline run and retries. The pod keeps the same name, and
+        # the previous events are still around.
+        state.mark_all_events_as_processed()
+        self.is_monitoring.set()
+
         while not self.stop_monitoring.is_set():
-            # Loop until told to stop
-            # We wait for events up to 10 seconds, passing the resource version from the last batch of events each time,
-            # this ensures that we only receive new events.
-            # https://stackoverflow.com/questions/72133783/how-to-avoid-resource-too-old-when-retrying-a-watch-with-kubernetes-python-cli
-            try:
-                for event in w.stream(
-                    api.list_namespaced_event,
-                    namespace=self.namespace,
-                    timeout_seconds=10,
-                    resource_version=resource_version,
-                ):
-                    if not isinstance(event, dict):
-                        logger.warn(
-                            f"Attempt to process an event that was type {type(event)} when dict was expected. {event}"
-                        )
-                        continue
+            # We get a performance benefit from streaming and processing
+            # new events as they happen, rather than constantly polling.
+            # However watches timeout, and we need to wake up often to
+            # see if we should stop. So this logic streams and processes
+            # new events, and then after the timeout reloads
+            for core_event in state.stream_unprocessed_events(api, timeout=20):
+                self._process_event(core_event)
+            state.reload_event_list(api)
 
-                    core_event = event["object"]
-                    resource_version = core_event.metadata.resource_version
-                    if self._is_relevant(core_event):
-                        logger.log(
-                            EventLogger._set_log_level(core_event),
-                            EventLogger._build_msg(core_event),
-                        )
-
-                if self.stop_monitoring.is_set():
-                    w.stop()
-            except ApiException as e:
-                # It's possible that the watched version might not be valid anymore, since watches are only
-                # kept around for a short period of time. If that happens, retrieve list of events again.
-                # We poll often enough that this probably doesn't happen in practice.
-                if e.status == http.client.GONE:
-                    logger.debug(
-                        f"Watch API return GONE while monitoring events {self.involved_objects}"
-                    )
-                    resource_version = None
-                else:
-                    raise
         logger.debug(f"Done monitoring events for {self.involved_objects}")
 
     def __enter__(self):
         logger.debug(f"Starting event Logger Process")
         self.process.start()
+        self.is_monitoring.wait(60)
         logger.debug(
             f"Event Logger Process {self.process.pid} has started for objects {self.involved_objects}"
         )
