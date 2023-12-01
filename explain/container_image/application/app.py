331a9a285f1f3b44f55d2524904a572bceb16a5d
Nick Lawrence <ntl@us.ibm.com>
2023-12-01 12:05:38 -0600

    fix slideshow

--- explain/container_image/application/app.py
+++ explain/container_image/application/app.py
@@ -1,7 +1,22 @@
+# Copyright 2023 IBM All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from flask import Flask, Response, render_template
 import re
 import accounts
 import http
+from database import DBConnection
 
 app = Flask(__name__, instance_relative_config=True)
 app.register_blueprint(accounts.bp)
@@ -14,7 +29,7 @@ def alive():
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("home.jinja")
+    return render_template("home.jinja", database=DBConnection.__name__)
 
 
 @app.template_global(name="human_readable")
--- explain/container_image/application/database.py
+++ explain/container_image/application/database.py
@@ -1,9 +1,39 @@
+# Copyright 2023 IBM All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Defines database connection classes.
+
+The variable "DBConnection" points to the class that should be used, given the connection details supplied
+by environment variables.
+
+For example:
+
+from database import DBConnection
+
+with DBConnection() as db:
+    accounts = db.get_accounts(offset=offset, limit=limit)
+    num_accounts = db.get_number_of_accounts()
+
+"""
 import ibm_db
 import json
 import os
 from typing import List, Dict, Union, Generator, Any
 import logging
 import psycopg
+from enum import Enum
 
 logger = logging.getLogger(__name__)
 
