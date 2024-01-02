23e1f3469a71cfed90727fec2da9cb9e86204f12
Nick Lawrence <ntl@us.ibm.com>
2024-01-02 11:23:15 -0600

    yolo

--- distributed_training/pytorch/yolov5/src/download.py
+++ distributed_training/pytorch/yolov5/src/download.py
@@ -6,22 +6,4 @@ import os
 with open("./data.yaml") as f:
     cfg = yaml.safe_load(f.read())
 
-# Download labels
-segments = True  # segment or box labels
-dir = Path(cfg["path"])  # dataset root dir
-
-if os.path.exists(dir):
-    exit(0)
-
-url = "https://github.com/ultralytics/yolov5/releases/download/v1.0/"
-urls = [
-    url + ("coco2017labels-segments.zip" if segments else "coco2017labels.zip")
-]  # labels
-download(urls, dir=dir.parent)
-# Download data
-urls = [
-    "http://images.cocodataset.org/zips/train2017.zip",  # 19G, 118k images
-    "http://images.cocodataset.org/zips/val2017.zip",  # 1G, 5k images
-    "http://images.cocodataset.org/zips/test2017.zip",
-]  # 7G, 41k images (optional)
-download(urls, dir=dir / "images", threads=3)
+download("https://ultralytics.com/assets/coco128.zip", dir=Path(cfg["path"]), threads=3)
