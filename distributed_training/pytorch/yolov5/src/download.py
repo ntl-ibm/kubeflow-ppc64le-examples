162299afeff16dadec57a7c5711bdccd0dfa46fd
Nick Lawrence <ntl@us.ibm.com>
2024-01-02 13:36:42 -0600

    yolo

--- distributed_training/pytorch/yolov5/src/download.py
+++ distributed_training/pytorch/yolov5/src/download.py
@@ -6,10 +6,10 @@ import os
 with open("./data.yaml") as f:
     cfg = yaml.safe_load(f.read())
 
-os.makedirs(cfg["path"], exist_ok=True)
+os.makedirs(cfg["path"].parent, exist_ok=True)
 download(
     "https://ultralytics.com/assets/coco128.zip",
-    dir=Path(cfg["path"]),
+    dir=Path(cfg["path"]).parent,
     unzip=True,
     delete=True,
 )
