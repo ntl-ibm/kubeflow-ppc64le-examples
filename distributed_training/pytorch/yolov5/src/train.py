b43fa72d90996f0f05512a272bfee95fade00c29
Nick Lawrence <ntl@us.ibm.com>
2024-01-02 17:35:09 -0600

    yolo

--- distributed_training/pytorch/yolov5/src/train.py
+++ distributed_training/pytorch/yolov5/src/train.py
@@ -3,15 +3,21 @@ import yaml
 import torch.distributed as dist
 import torch
 import os
-from ultralytics.utils import DEFAULT_CFG, LOGGER, RANK, colorstr
+from ultralytics.utils import DEFAULT_CFG, LOGGER, RANK, colorstr, TQDM, callbacks
 import subprocess
 from ultralytics.models import yolo
 import time
+import numpy as np
+import math
+import warnings
 
 # from ultralytics.utils.torch_utils import torch_distributed_zero_first
 from ultralytics.data import build_dataloader, build_yolo_dataset
 from datetime import datetime, timedelta
 from contextlib import contextmanager
+from ultralytics.utils.torch_utils import EarlyStopping, ModelEMA
+from ultralytics.utils.checks import check_amp, check_imgsz, check_train_batch_size
+from torch import nn
 
 LOCAL_RANK = int(os.environ["LOCAL_RANK"]) if "LOCAL_RANK" in os.environ else -1
 
@@ -71,6 +77,116 @@ class YoloDdpTrainer(yolo.detect.DetectionTrainer):
             rank=RANK,
             world_size=world_size,
         )
+
+    def _setup_train(self, world_size):
+        """Builds dataloaders and optimizer on correct rank process."""
+
+        # Model
+        self.run_callbacks("on_pretrain_routine_start")
+        ckpt = self.setup_model()
+        self.model = self.model.to(self.device)
+        self.set_model_attributes()
+
+        # Freeze layers
+        freeze_list = (
+            self.args.freeze
+            if isinstance(self.args.freeze, list)
+            else range(self.args.freeze)
+            if isinstance(self.args.freeze, int)
+            else []
+        )
+        always_freeze_names = [".dfl"]  # always freeze these layers
+        freeze_layer_names = [f"model.{x}." for x in freeze_list] + always_freeze_names
+        for k, v in self.model.named_parameters():
+            # v.register_hook(lambda x: torch.nan_to_num(x))  # NaN to 0 (commented for erratic training results)
+            if any(x in k for x in freeze_layer_names):
+                LOGGER.info(f"Freezing layer '{k}'")
+                v.requires_grad = False
+            elif not v.requires_grad:
+                LOGGER.info(
+                    f"WARNING ⚠️ setting 'requires_grad=True' for frozen layer '{k}'. "
+                    "See ultralytics.engine.trainer for customization of frozen layers."
+                )
+                v.requires_grad = True
+
+        # Check AMP
+        self.amp = torch.tensor(self.args.amp).to(self.device)  # True or False
+        if self.amp and RANK in (-1, 0):  # Single-GPU and DDP
+            callbacks_backup = (
+                callbacks.default_callbacks.copy()
+            )  # backup callbacks as check_amp() resets them
+            self.amp = torch.tensor(check_amp(self.model), device=self.device)
+            callbacks.default_callbacks = callbacks_backup  # restore callbacks
+        if RANK > -1 and world_size > 1:  # DDP
+            dist.broadcast(
+                self.amp, src=0
+            )  # broadcast the tensor from rank 0 to all other ranks (returns None)
+        self.amp = bool(self.amp)  # as boolean
+        self.scaler = torch.cuda.amp.GradScaler(enabled=self.amp)
+        if world_size > 1:
+            self.model = nn.parallel.DistributedDataParallel(
+                self.model, device_ids=[LOCAL_RANK]
+            )
+
+        # Check imgsz
+        gs = max(
+            int(self.model.stride.max() if hasattr(self.model, "stride") else 32), 32
+        )  # grid size (max stride)
+        self.args.imgsz = check_imgsz(self.args.imgsz, stride=gs, floor=gs, max_dim=1)
+
+        # Batch size
+        if (
+            self.batch_size == -1 and RANK == -1
+        ):  # single-GPU only, estimate best batch size
+            self.args.batch = self.batch_size = check_train_batch_size(
+                self.model, self.args.imgsz, self.amp
+            )
+
+        # Dataloaders
+        batch_size = self.batch_size // max(world_size, 1)
+        self.train_loader = self.get_dataloader(
+            self.trainset, batch_size=batch_size, rank=RANK, mode="train"
+        )
+        if RANK in (-1, 0):
+            self.test_loader = self.get_dataloader(
+                self.testset, batch_size=batch_size * 2, rank=-1, mode="val"
+            )
+            self.validator = self.get_validator()
+            metric_keys = self.validator.metrics.keys + self.label_loss_items(
+                prefix="val"
+            )
+            self.metrics = dict(zip(metric_keys, [0] * len(metric_keys)))
+            self.ema = ModelEMA(self.model)
+            if self.args.plots:
+                self.plot_training_labels()
+
+        # Optimizer
+        self.accumulate = max(
+            round(self.args.nbs / self.batch_size), 1
+        )  # accumulate loss before optimizing
+        weight_decay = (
+            self.args.weight_decay * self.batch_size * self.accumulate / self.args.nbs
+        )  # scale weight_decay
+        iterations = (
+            math.ceil(
+                len(self.train_loader.dataset) / max(self.batch_size, self.args.nbs)
+            )
+            * self.epochs
+        )
+        self.optimizer = self.build_optimizer(
+            model=self.model,
+            name=self.args.optimizer,
+            lr=self.args.lr0,
+            momentum=self.args.momentum,
+            decay=weight_decay,
+            iterations=iterations,
+        )
+        # Scheduler
+        self._setup_scheduler()
+        self.stopper, self.stop = EarlyStopping(patience=self.args.patience), False
+        self.resume_training(ckpt)
+        self.scheduler.last_epoch = self.start_epoch - 1  # do not move
+        self.run_callbacks("on_pretrain_routine_end")
 
     def _do_train(self, world_size=1):
         """Train completed, evaluate and plot if specified by arguments."""
