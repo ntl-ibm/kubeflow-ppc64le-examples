47999e53fcb8de45126d99e487050a4266674640
Nick Lawrence <ntl@us.ibm.com>
2024-01-02 13:57:15 -0600

    yolo

--- distributed_training/pytorch/yolov5/src/download.py
+++ distributed_training/pytorch/yolov5/src/download.py
@@ -2,9 +2,10 @@ from ultralytics.utils.downloads import download
 from pathlib import Path
 import yaml
 import os
+import stat
 
 download("https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt")
-
+os.chmod("yolov8n.pt", stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)
 with open("./data.yaml") as f:
     cfg = yaml.safe_load(f.read())
 
