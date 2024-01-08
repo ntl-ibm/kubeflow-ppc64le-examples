c06cb5f86f568133c80f3dc5a3f2448b23f65e4f
Nick Lawrence <ntl@us.ibm.com>
2024-01-08 09:45:29 -0600

    fixes

--- distributed_training/pytorch/yolo/src/build_model.py
+++ distributed_training/pytorch/yolo/src/build_model.py
@@ -40,6 +40,8 @@ results = model.train(
     data="./data.yaml",
     cfg="./train.yaml",
     trainer=YoloDdpDetectTrainer if RANK != -1 else None,
+    logdir="/workspace/tensorboard_logs",
+    loggers="tensorboard",
 )
 if RANK in (-1, 0):
     # Save test metrics for the model
