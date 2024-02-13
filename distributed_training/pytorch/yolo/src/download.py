c589cfc046a55b6a6efc8cfd60c7359f829f836a
Nick Lawrence <ntl@us.ibm.com>
2024-02-13 07:17:34 -0600

    future demo prep

--- distributed_training/pytorch/yolo/src/download.py
+++ distributed_training/pytorch/yolo/src/download.py
@@ -31,9 +31,11 @@ with open("./data.yaml") as f:
     cfg = yaml.safe_load(f.read())
 
 # Download training and validation data
+# Normally the link is here "https://ultralytics.com/assets/coco128.zip"
+# Due to firewall issues in some environments, the zip is included in my github repo
 os.makedirs(Path(cfg["path"]).parent, exist_ok=True)
 download(
-    "https://ultralytics.com/assets/coco128.zip",
+    "https://github.com/ntl-ibm/kubeflow-ppc64le-examples/raw/3.0.0/distributed_training/pytorch/yolo/assets/coco128.zip",
     dir=Path(cfg["path"]).parent,
     unzip=True,
     delete=True,
